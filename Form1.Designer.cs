84b9f0fed442127cf201be57b7b9b0086e2c1385
aksmaxn4 <70766698+aksmaxn4@users.noreply.github.com>
2020-09-06 14:36:04 +0500

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace OptimalInvestment2._1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3});
+            this.dataGridView1.Location = new System.Drawing.Point(33, 31);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(166, 116);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(710, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(155, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Сколько вы хотите вложить?";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(713, 60);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(152, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextBox1_KeyPress);
+            this.textBox1.KeyUp += new System.Windows.Forms.KeyEventHandler(this.TextBox1_KeyUp);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(713, 86);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Вложить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(87, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Проекты";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(710, 125);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "label3";
+            this.label3.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 63);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(15, 52);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "В\r\nл\r\nо\r\nж";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button5);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Location = new System.Drawing.Point(582, 225);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(283, 97);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Управление таблицей";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(6, 19);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(138, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Добавить проект";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(155, 19);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(120, 23);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Удалить проект";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(6, 58);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(138, 23);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "Добавить вложения";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(155, 58);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(121, 23);
+            this.button5.TabIndex = 11;
+            this.button5.Text = "Удалить вложения";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.Button5_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(514, 31);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(190, 168);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Visible = false;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "1";
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 40;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "2";
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 40;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "3";
+            this.Column3.Name = "Column3";
+            this.Column3.Width = 40;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(879, 334);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,257 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OptimalInvestment2._1
+{
+    public partial class Form1 : Form
+    {
+
+        double[,] start_mas = null;
+        double[,,] mas = null;
+
+        int column_count = 3;
+        int rows_count = 4;
+        int x = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            dataGridView1.Rows.Add(4);
+
+            for (int i = 0; (i <= (dataGridView1.Rows.Count - 1)); i++)
+            {
+                dataGridView1.Rows[i].HeaderCell.Value = string.Format((i).ToString(), "0");
+            }
+            InitializeMas();
+            button1.Enabled = false;
+
+
+        }
+
+        public bool GetStarted()
+        {
+            bool ok = true;
+            for (int i = 0; i < start_mas.GetLength(0); i++)
+            {
+                for (int j = 0; j < start_mas.GetLength(1); j++)
+                {
+                    try
+                    {
+                        start_mas[i, j] = Convert.ToDouble(dataGridView1.Rows[i].Cells[j].Value);
+                        if (start_mas[i, j] < 0) throw new Exception();
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Проверьте введенные в таблице данные\nДолжны быть только положительные целые и вещественные числа");
+
+                        ok = false;
+                        break;
+
+                    }
+                }
+                if (ok == false) break;
+            }
+
+            return ok;
+
+        }
+
+        public void FirstNSecondMaxValue(int A = 0)
+        {
+            if (A <= dataGridView1.Rows.Count - 1)
+            {
+                double sum = 0; double curr = 0;
+                for (int x = 0; x <= A; x++)
+                {
+                    curr = start_mas[x, 0] + start_mas[A - x, 1];
+                    if (curr >= sum)
+                    {
+
+
+                        sum = curr;
+                        mas[A, 0, 0] = curr;
+                        mas[A, 0, 1] = x;
+                        mas[A, 0, 2] = A - x;
+
+                    }
+                }
+                FirstNSecondMaxValue(++A);
+            }
+            else if (dataGridView1.ColumnCount > 2) NextGenMaxValue(2, 0);
+            else return;
+
+
+
+        }
+        public void NextGenMaxValue(int columnStart, int columnMas, int A = 0)
+        {
+            if (A <= dataGridView1.Rows.Count - 1)
+            {
+                double sum = 0; double curr = 0;
+                for (int x = 0; x <= A; x++)
+                {
+                    curr = mas[x, columnMas, 0] + start_mas[A - x, columnStart];
+                    if (curr >= sum)
+                    {
+                        sum = curr;
+                        mas[A, columnMas + 1, 0] = curr;
+                        for (int k = 1; k < mas.GetLength(2); k++)
+                        {
+                            mas[A, columnMas + 1, k] = mas[x, columnMas, k];
+                        }
+                        mas[A, columnMas + 1, columnStart + 1] = A - x;
+
+                    }
+                }
+                NextGenMaxValue(columnStart, columnMas, ++A);
+            }
+            else if (columnStart + 1 < start_mas.GetLength(1)) NextGenMaxValue(++columnStart, ++columnMas);
+            else return;
+
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+
+            x = Convert.ToInt32(textBox1.Text);
+            if (x > dataGridView1.Rows.Count - 1)
+            {
+                MessageBox.Show("Вложения не могут быть больше максимального значения левого столбца");
+                return;
+            }
+            else
+            {
+                if (GetStarted() == true)
+                {
+
+                    ClearGrid();
+                    Zero();
+                    FirstNSecondMaxValue();
+                    Finish();
+                    pictureBox1.Visible = true;
+                }
+            }
+
+        }
+
+        void AddColumn()
+        {
+            column_count++;
+            dataGridView1.Width += 40;
+            dataGridView1.Columns.Add("Column " + column_count.ToString(), column_count.ToString());
+            dataGridView1.Columns[column_count - 1].Width = 40;
+            label2.Location = new Point(dataGridView1.Width / 2, 13);
+            InitializeMas();
+
+
+        }
+        void DeleteColumn()
+        {
+            column_count--;
+            dataGridView1.Width -= 40;
+            dataGridView1.Columns.RemoveAt(column_count);
+            label2.Location = new Point(dataGridView1.Width / 2, 13);
+            InitializeMas();
+        }
+        void AddRows()
+        {
+            int x = label4.Location.X; int y = label4.Location.Y;
+            dataGridView1.Rows.Add();
+            dataGridView1.Rows[rows_count].HeaderCell.Value = rows_count.ToString();
+            dataGridView1.Height += 22;
+            label4.Location = new Point(x, dataGridView1.Height / 2);
+            rows_count++;
+            InitializeMas();
+        }
+        void DeleteRows()
+        {
+            int x = label4.Location.X; int y = label4.Location.Y;
+            dataGridView1.Rows.RemoveAt(rows_count - 1);
+            dataGridView1.Height -= 22;
+            label4.Location = new Point(x, dataGridView1.Height / 2);
+            rows_count--;
+            InitializeMas();
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            button3.Enabled = true;
+            AddColumn();
+            if (column_count == 10) button2.Enabled = false;
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            button2.Enabled = true;
+            DeleteColumn();
+            if (column_count == 2) button3.Enabled = false;
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            button5.Enabled = true;
+            AddRows();
+            if (rows_count == 10) button4.Enabled = false;
+        }
+
+        private void Button5_Click(object sender, EventArgs e)
+        {
+            button4.Enabled = true;
+            DeleteRows();
+            if (rows_count == 2) button5.Enabled = false;
+
+        }
+
+        private void InitializeMas()
+        {
+            start_mas = new double[dataGridView1.Rows.Count, dataGridView1.ColumnCount];
+            mas = new double[dataGridView1.Rows.Count, dataGridView1.Columns.Count - 1, dataGridView1.Columns.Count + 1];
+        }
+        private void Finish()
+        {
+            int i = 0;
+            label3.Text = "Максимальная прибыль:" + "\n" + mas[x, mas.GetLength(1) - 1, 0];
+            label3.Visible = true;
+            for (int k = 1; k < mas.GetLength(2); k++)
+            {
+                dataGridView1[i, Convert.ToInt32(mas[x, mas.GetLength(1) - 1, k])].Style.BackColor = System.Drawing.Color.Red;
+                i++;
+            }
+        }
+        private void ClearGrid()
+        {
+            for (int i = 0; i < dataGridView1.ColumnCount; i++)
+                for (int j = 0; j < dataGridView1.Rows.Count; j++)
+                    dataGridView1[i, j].Style.BackColor = System.Drawing.Color.White;
+
+        }
+
+        private void TextBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar) && e.KeyChar != (char)Keys.Back)
+                e.Handled = true;
+        }
+
+        private void TextBox1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (textBox1.Text.Length > 0) button1.Enabled = true;
+            else button1.Enabled = false;
+        }
+        private void Zero()
+        {
+            for (int i = 0; i < dataGridView1.ColumnCount; i++)
+                for (int j = 0; j < dataGridView1.Rows.Count; j++)
+                    if (dataGridView1[i, j].Value == null) dataGridView1[i, j].Value = "0";
+
+
+        }
+    }
+
+}
